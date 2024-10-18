025ab2e5c866c2300bec165664c3c2735b494372
xsteadybcgo <xw4585@gmail.com>
2024-10-18 17:21:20 +0800

    Initialize subgraph

---
+++ zkl-holder/src/zk-link-token.ts
@@ -0,0 +1,133 @@
+import {
+  AdminChanged as AdminChangedEvent,
+  Approval as ApprovalEvent,
+  BeaconUpgraded as BeaconUpgradedEvent,
+  EIP712DomainChanged as EIP712DomainChangedEvent,
+  Initialized as InitializedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent,
+  Upgraded as UpgradedEvent
+} from "../generated/ZkLinkToken/ZkLinkToken"
+import {
+  AdminChanged,
+  Approval,
+  BeaconUpgraded,
+  EIP712DomainChanged,
+  Initialized,
+  OwnershipTransferred,
+  Transfer,
+  Upgraded
+} from "../generated/schema"
+
+export function handleAdminChanged(event: AdminChangedEvent): void {
+  let entity = new AdminChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousAdmin = event.params.previousAdmin
+  entity.newAdmin = event.params.newAdmin
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
+  let entity = new BeaconUpgraded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.beacon = event.params.beacon
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleEIP712DomainChanged(
+  event: EIP712DomainChangedEvent
+): void {
+  let entity = new EIP712DomainChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInitialized(event: InitializedEvent): void {
+  let entity = new Initialized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.version = event.params.version
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUpgraded(event: UpgradedEvent): void {
+  let entity = new Upgraded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.implementation = event.params.implementation
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ zkl-holder/tests/zk-link-token-utils.ts
@@ -0,0 +1,150 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  AdminChanged,
+  Approval,
+  BeaconUpgraded,
+  EIP712DomainChanged,
+  Initialized,
+  OwnershipTransferred,
+  Transfer,
+  Upgraded
+} from "../generated/ZkLinkToken/ZkLinkToken"
+
+export function createAdminChangedEvent(
+  previousAdmin: Address,
+  newAdmin: Address
+): AdminChanged {
+  let adminChangedEvent = changetype<AdminChanged>(newMockEvent())
+
+  adminChangedEvent.parameters = new Array()
+
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousAdmin",
+      ethereum.Value.fromAddress(previousAdmin)
+    )
+  )
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam("newAdmin", ethereum.Value.fromAddress(newAdmin))
+  )
+
+  return adminChangedEvent
+}
+
+export function createApprovalEvent(
+  owner: Address,
+  spender: Address,
+  value: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("spender", ethereum.Value.fromAddress(spender))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return approvalEvent
+}
+
+export function createBeaconUpgradedEvent(beacon: Address): BeaconUpgraded {
+  let beaconUpgradedEvent = changetype<BeaconUpgraded>(newMockEvent())
+
+  beaconUpgradedEvent.parameters = new Array()
+
+  beaconUpgradedEvent.parameters.push(
+    new ethereum.EventParam("beacon", ethereum.Value.fromAddress(beacon))
+  )
+
+  return beaconUpgradedEvent
+}
+
+export function createEIP712DomainChangedEvent(): EIP712DomainChanged {
+  let eip712DomainChangedEvent = changetype<EIP712DomainChanged>(newMockEvent())
+
+  eip712DomainChangedEvent.parameters = new Array()
+
+  return eip712DomainChangedEvent
+}
+
+export function createInitializedEvent(version: i32): Initialized {
+  let initializedEvent = changetype<Initialized>(newMockEvent())
+
+  initializedEvent.parameters = new Array()
+
+  initializedEvent.parameters.push(
+    new ethereum.EventParam(
+      "version",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(version))
+    )
+  )
+
+  return initializedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  value: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return transferEvent
+}
+
+export function createUpgradedEvent(implementation: Address): Upgraded {
+  let upgradedEvent = changetype<Upgraded>(newMockEvent())
+
+  upgradedEvent.parameters = new Array()
+
+  upgradedEvent.parameters.push(
+    new ethereum.EventParam(
+      "implementation",
+      ethereum.Value.fromAddress(implementation)
+    )
+  )
+
+  return upgradedEvent
+}
